6df089b25f0499e06d635a700514f4220ac30962
Babithkp <babithkullachetti44@gmail.com>
2025-09-02 19:58:15 +0530

    sub header content has been changed

--- src/components/Landing/HeroSection.tsx
+++ src/components/Landing/HeroSection.tsx
@@ -8,7 +8,7 @@ import RightVector from "@/assets/RightVector";
 
 export default function HeroSection() {
   return (
-    <section className="relative flex flex-col items-center justify-center gap-16 max-lg:overflow-hidden ">
+    <section className="relative flex flex-col items-center justify-center gap-16 max-lg:overflow-hidden">
       <div className="absolute left-0 blur-xl">
         <LeftVector />
       </div>
@@ -16,7 +16,7 @@ export default function HeroSection() {
         <RightVector />
       </div>
       <div className="flex flex-col items-center gap-8 pt-30 text-center">
-        <h1 className="flex w-200 flex-wrap items-center justify-center gap-3 text-5xl font-bold max-lg:text-4xl max-md:text-3xl max-sm:text-xl max-sm:w-full">
+        <h1 className="flex w-200 flex-wrap items-center justify-center gap-3 text-5xl font-bold max-lg:text-4xl max-md:text-3xl max-sm:w-full max-sm:text-xl">
           Future-Proof
           <span
             style={{
@@ -31,12 +31,12 @@ export default function HeroSection() {
           </span>
           Designed to Grow Your Business
         </h1>
-        <h3 className="w-200 text-lg max-lg:text-base max-md:text-sm max-md:w-150 max-sm:w-[90%]">
-          We specialise in building tailor-made websites, web apps, and digital
-          products. With a focus on clean UI/UX design and robust development,
-          we deliver seamless, scalable solutions for modern businesses.
+        <h3 className="w-200 text-lg max-lg:text-base max-md:w-150 max-md:text-sm max-sm:w-[90%]">
+          We craft tailor-made websites and apps with clean UI/UX design,
+          scalable development, and future-ready strategies, helping your
+          business stand out and grow online.
         </h3>
-        <div className="z-1 flex gap-5 w-full justify-center">
+        <div className="z-1 flex w-full justify-center gap-5">
           <Button
             variant={"outline"}
             className="border border-black px-7 hover:bg-black hover:text-white"
@@ -49,29 +49,29 @@ export default function HeroSection() {
           </Button>
         </div>
       </div>
-      <div className="flex w-[70%] flex-wrap items-start justify-between px-20 max-lg:px-5 max-md:px-0 max-lg:w-full gap-5">
-        <div className="flex w-45 flex-col items-center gap-3 text-center text-lg max-sm:text-sm ">
+      <div className="flex w-[70%] flex-wrap items-start justify-between gap-5 px-20 max-lg:w-full max-lg:px-5 max-md:px-0">
+        <div className="flex w-45 flex-col items-center gap-3 text-center text-lg max-sm:text-sm">
           <Brain />
           <p className="font-[600]">Design That Thinks</p>
           <p className="text-sm font-light max-sm:text-xs">
             Ideas before interfaces. We solve before we style.
           </p>
         </div>
-        <div className="flex w-45 flex-col items-center gap-3 text-center text-lg max-sm:text-sm ">
+        <div className="flex w-45 flex-col items-center gap-3 text-center text-lg max-sm:text-sm">
           <Finger />
           <p className="font-[600]">Tech That Feels</p>
           <p className="text-sm font-light max-sm:text-xs">
             Interfaces that understand users, not just instruct them.
           </p>
         </div>
-        <div className="flex w-45 flex-col items-center gap-3 text-center text-lg max-sm:text-sm ">
+        <div className="flex w-45 flex-col items-center gap-3 text-center text-lg max-sm:text-sm">
           <Code />
           <p className="font-[600]">Code That Clicks</p>
           <p className="text-sm font-light max-sm:text-xs">
             Structured, scalable, and clean — every line matters.
           </p>
         </div>
-        <div className="flex w-45 flex-col items-center gap-3 text-center text-lg max-sm:text-sm ">
+        <div className="flex w-45 flex-col items-center gap-3 text-center text-lg max-sm:text-sm">
           <Strategy />
           <p className="font-[600]">Strategy That Sticks</p>
           <p className="text-sm font-light max-sm:text-xs">
